c0f27fd4f5cc122f77984a3a87cdf5e476e0cbbd
Takahiro Ishikawa <qwqkx447@yahoo.co.jp>
2015-01-10 13:17:35 +0900

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by Takahiro on 2015/01/10.
+//  Copyright (c) 2015年 石川 貴大. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
